b2a17bd7ce15ccb9bc0387a7b4bd957ca1e8f60c
David Sousa <david01sousa@hotmail.com>
2023-10-30 20:22:57 -0300

    feat: add env treatment and express

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { env } from "./env";
+
+const port = env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => console.log(`running on port ${port}! 🚀`));
