91cfef56e55219dbd0a4c4f76f780296f083cc7a
David Sousa <david01sousa@hotmail.com>
2023-11-01 19:35:51 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { env } from "./env";
 import { AppDataSource } from "./db/data-source";
 import routes from "./routes";
+import cors from "cors";
 
 AppDataSource.initialize().then(() => {
   const port = env.PORT;
 
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
   app.use(routes);
 
